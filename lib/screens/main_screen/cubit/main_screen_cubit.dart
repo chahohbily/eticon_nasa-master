f336d25820c3d2c771ccc53b9217ae5c82d2779b
chahohbily <90547058+chahohbily@users.noreply.github.com>
2021-11-08 19:51:07 +0700

    Update main_screen_cubit.dart

--- lib/screens/main_screen/cubit/main_screen_cubit.dart
+++ lib/screens/main_screen/cubit/main_screen_cubit.dart
@@ -5,7 +5,7 @@ import '../../../models/apod_model.dart';
 import 'main_screen_state.dart';
 
 class MainScreenCubit extends Cubit<MainScreenState> {
-  late List<ApodModel> apodModel;
+  List<ApodModel> apodModel = [];
   MainScreenCubit() : super(MainScreenLoading());
 
   Future<void> getData() async {
@@ -16,7 +16,10 @@ class MainScreenCubit extends Cubit<MainScreenState> {
             'sol': '1000',
             'api_key': 'hTyuBrb3xGcFIbQkGlsb8aMPurTJjQKuB20eAe65'
           });
-      emit(MainScreenLoaded(result: ApodModel.fromJson(response)));
+      for (Map<String, dynamic> model in response['photos']){
+        apodModel.add(ApodModel.fromJson(model));
+      }
+      emit(MainScreenLoaded(result: apodModel));
     } on APIException catch (error) {
       if (error.code == 0) {
         emit(MainScreenError(errorMsg: 'No internet connection...'));
