3b940eac9c12086ce4d8558d3833dca982e23dd3
chahohbily <90547058+chahohbily@users.noreply.github.com>
2021-11-08 19:53:54 +0700

    Update detail_screen.dart

--- lib/screens/main_screen/detail_screen.dart
+++ lib/screens/main_screen/detail_screen.dart
@@ -4,16 +4,15 @@ import '../../models/apod_model.dart';
 
 class DetailScreen extends StatelessWidget {
   final ApodModel apodModel;
-  int index;
 
-  DetailScreen({required this.apodModel, required this.index});
+  DetailScreen({required this.apodModel});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          '${apodModel.photos[index].camera.name} ${apodModel.photos[index].id}',
+          '${apodModel.camera!.name!} ${apodModel.id}',
           style: TextStyle(color: Colors.white),
         ),
         centerTitle: true,
@@ -24,9 +23,12 @@ class DetailScreen extends StatelessWidget {
           Container(
             height: 275,
             width: 420,
-            child: Image.network(
-              apodModel.photos[index].imgSrc,
-              fit: BoxFit.fill,
+            child: Hero(
+              tag: apodModel.id.toString(),
+              child: Image.network(
+                apodModel.imgSrc!,
+                fit: BoxFit.fill,
+              ),
             ),
           ),
           SizedBox(
@@ -47,17 +49,17 @@ class DetailScreen extends StatelessWidget {
                       fontSize: 20,
                       fontWeight: FontWeight.bold),
                 ),
-                detailInfo('Id', '${apodModel.photos[index].camera.id}'),
+                detailInfo('Id', '${apodModel.camera!.id!}'),
                 SizedBox(
                   height: 3,
                 ),
                 detailInfo(
-                    'Full name', '${apodModel.photos[index].camera.fullName}'),
+                    'Full name', '${apodModel.camera!.fullName!}'),
                 SizedBox(
                   height: 3,
                 ),
                 detailInfo(
-                    'Rover id', '${apodModel.photos[index].camera.roverId}')
+                    'Rover id', '${apodModel.camera!.roverId!}')
               ],
             ),
             decoration: BoxDecoration(
@@ -89,21 +91,21 @@ class DetailScreen extends StatelessWidget {
                       fontSize: 20,
                       fontWeight: FontWeight.bold),
                 ),
-                detailInfo('Name', '${apodModel.photos[index].rover.name}'),
+                detailInfo('Name', '${apodModel.rover!.name!}'),
                 SizedBox(
                   height: 3,
                 ),
                 detailInfo('Launch date',
-                    '${apodModel.photos[index].rover.launchDate}'),
+                    '${apodModel.rover!.launchDate!}'),
                 SizedBox(
                   height: 3,
                 ),
                 detailInfo('Landing date',
-                    '${apodModel.photos[index].rover.landingDate}'),
+                    '${apodModel.rover!.landingDate!}'),
                 SizedBox(
                   height: 3,
                 ),
-                detailInfo('Status', '${apodModel.photos[index].rover.status}'),
+                detailInfo('Status', '${apodModel.rover!.status!}'),
               ],
             ),
             decoration: BoxDecoration(
