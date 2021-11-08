1afc7f90ddf368ad7db90b5a34ffd8b0013be220
chahohbily <90547058+chahohbily@users.noreply.github.com>
2021-11-08 19:59:22 +0700

    Update apod_model.dart

--- lib/models/apod_model.dart
+++ lib/models/apod_model.dart
@@ -1,50 +1,27 @@
 class ApodModel {
-  late List<Photos> photos;
+  int? id;
+  int? sol;
+  Camera? camera;
+  String? imgSrc;
+  String? earthDate;
+  Rover? rover;
 
-  ApodModel({required this.photos});
+  ApodModel(
+      {this.id,
+        this.sol,
+        this.camera,
+        this.imgSrc,
+        this.earthDate,
+        this.rover});
 
   ApodModel.fromJson(Map<String, dynamic> json) {
-    if (json['photos'] != null) {
-      photos = <Photos>[];
-      json['photos'].forEach((v) {
-        photos.add(new Photos.fromJson(v));
-      });
-    }
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    if (this.photos != null) {
-      data['photos'] = this.photos.map((v) => v.toJson()).toList();
-    }
-    return data;
-  }
-}
-
-class Photos {
-  late int id;
-  late int sol;
-  late Camera camera;
-  late String imgSrc;
-  late String earthDate;
-  late Rover rover;
-
-  Photos(
-      {required this.id,
-        required this.sol,
-        required this.camera,
-        required this.imgSrc,
-        required this.earthDate,
-        required this.rover});
-
-  Photos.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     sol = json['sol'];
     camera =
-    (json['camera'] != null ? new Camera.fromJson(json['camera']) : null)!;
+    json['camera'] != null ? new Camera.fromJson(json['camera']) : null;
     imgSrc = json['img_src'];
     earthDate = json['earth_date'];
-    rover = (json['rover'] != null ? new Rover.fromJson(json['rover']) : null)!;
+    rover = json['rover'] != null ? new Rover.fromJson(json['rover']) : null;
   }
 
   Map<String, dynamic> toJson() {
@@ -52,24 +29,24 @@ class Photos {
     data['id'] = this.id;
     data['sol'] = this.sol;
     if (this.camera != null) {
-      data['camera'] = this.camera.toJson();
+      data['camera'] = this.camera!.toJson();
     }
     data['img_src'] = this.imgSrc;
     data['earth_date'] = this.earthDate;
     if (this.rover != null) {
-      data['rover'] = this.rover.toJson();
+      data['rover'] = this.rover!.toJson();
     }
     return data;
   }
 }
 
 class Camera {
-  late int id;
-  late String name;
-  late int roverId;
-  late String fullName;
+  int? id;
+  String? name;
+  int? roverId;
+  String? fullName;
 
-  Camera({required this.id, required this.name, required this.roverId, required this.fullName});
+  Camera({this.id, this.name, this.roverId, this.fullName});
 
   Camera.fromJson(Map<String, dynamic> json) {
     id = json['id'];
@@ -89,13 +66,13 @@ class Camera {
 }
 
 class Rover {
-  late int id;
-  late String name;
-  late String landingDate;
-  late String launchDate;
-  late String status;
+  int? id;
+  String? name;
+  String? landingDate;
+  String? launchDate;
+  String? status;
 
-  Rover({required this.id, required this.name, required this.landingDate, required this.launchDate, required this.status});
+  Rover({this.id, this.name, this.landingDate, this.launchDate, this.status});
 
   Rover.fromJson(Map<String, dynamic> json) {
     id = json['id'];
@@ -115,3 +92,4 @@ class Rover {
     return data;
   }
 }
+
