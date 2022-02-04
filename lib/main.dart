2e32d0d954b47c79a6cf3b21501c613e0eef33e5
chahohbily <gabblegaffe@gmail.com>
2022-02-04 19:36:21 +0700

    token_add

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'screens/main_screen/main_screen_provider.dart';
 
 void main() {
   Api.init(baseUrl: 'https://api.nasa.gov/mars-photos/api/v1/');
+  Api.setToken('LB3wLcI40cV8FJ2il0PAICqDleXIeR2Pdo7I5avM');
   runApp(MyApp());
 }
 
